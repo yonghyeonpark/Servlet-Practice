87d9073ac7e10ad260051c9b4b74e3242360c6dc
YongHyeon <pyh4616@naver.com>
2023-12-12 05:02:42 +0900

    http 요청 메시지를 통한 서버로의 데이터 전달 방법 3가지

---
+++ src/main/java/practice/servlet/basic/BasicData.java
@@ -0,0 +1,12 @@
+package practice.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class BasicData {
+
+    private String username;
+    private int age;
+}
---
+++ src/main/java/practice/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,26 @@
+package practice.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletInputStream inputStream = request.getInputStream(); // 메시지 바디의 내용을 바이트 코드로 얻음
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+
+        response.getWriter().write("ok");
+    }
+}
