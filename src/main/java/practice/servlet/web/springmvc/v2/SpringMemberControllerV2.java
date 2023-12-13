50c0bd75f54cf640d92b99e949f4570a607d327c
YongHyeon <pyh4616@naver.com>
2023-12-14 06:17:48 +0900

    스프링 mvc 컨트롤러 통합.v2

---
+++ src/main/java/practice/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -0,0 +1,44 @@
+package practice.servlet.web.springmvc.v2;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import practice.servlet.domain.member.Member;
+import practice.servlet.domain.member.MemberRepository;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v2/members")
+public class SpringMemberControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/new-form")
+    public ModelAndView newForm() {
+        return new ModelAndView("new-form");
+    }
+
+    @RequestMapping("/save")
+    public ModelAndView save(HttpServletRequest request) {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelAndView modelAndView = new ModelAndView("save-result");
+        modelAndView.addObject("member", member);
+        return modelAndView;
+    }
+
+    @RequestMapping
+    public ModelAndView members() {
+        List<Member> members = memberRepository.findAll();
+
+        ModelAndView modelAndView = new ModelAndView("members");
+        modelAndView.addObject("members", members);
+        return modelAndView;
+    }
+}
