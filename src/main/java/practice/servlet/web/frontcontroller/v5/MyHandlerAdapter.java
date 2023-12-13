49dcd06e288b033b878a97d5103ed8d4a5fd60ab
YongHyeon <pyh4616@naver.com>
2023-12-14 03:38:45 +0900

    mvc 프레임워크 만들기 - 유연한 컨트롤러.v5

---
+++ src/main/java/practice/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -0,0 +1,15 @@
+package practice.servlet.web.frontcontroller.v5;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import practice.servlet.web.frontcontroller.ModelView;
+
+import java.io.IOException;
+
+public interface MyHandlerAdapter {
+
+    boolean supports(Object handler);
+
+    ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException;
+}
