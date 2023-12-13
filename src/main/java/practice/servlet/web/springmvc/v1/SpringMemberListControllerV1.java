d2b4ac38de25f6429693fc81d7b5e26801f6a575
YongHyeon <pyh4616@naver.com>
2023-12-14 06:10:04 +0900

    스프링 mvc 시작.v1

---
+++ src/main/java/practice/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,24 @@
+package practice.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import practice.servlet.domain.member.Member;
+import practice.servlet.domain.member.MemberRepository;
+
+import java.util.List;
+
+@Controller
+public class SpringMemberListControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process() {
+        List<Member> members = memberRepository.findAll();
+
+        ModelAndView modelAndView = new ModelAndView("members");
+        modelAndView.addObject("members", members);
+        return modelAndView;
+    }
+}
---
+++ src/main/java/practice/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -0,0 +1,27 @@
+package practice.servlet.web.springmvc.v1;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import practice.servlet.domain.member.Member;
+import practice.servlet.domain.member.MemberRepository;
+
+@Controller
+public class SpringMemberSaveControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members/save")
+    public ModelAndView process(HttpServletRequest request) {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelAndView modelAndView = new ModelAndView("save-result");
+        modelAndView.addObject("member", member);
+        return modelAndView;
+    }
+}
